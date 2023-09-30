d0a742e71bc434f13f02c1361f4c87affa74637a
aaaa922 <143300260+aaaa922@users.noreply.github.com>
2023-10-01 00:35:10 +0400

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,26 +4,58 @@ import { getByProps } from 'enmity/metro';
 import { create } from 'enmity/patcher';
 import manifest from '../manifest.json';
 
+import Discord from 'discord.js';
+
 import Settings from './components/Settings';
 
 const Typing = getByProps('startTyping');
 const Patcher = create('silent-typing');
 
 const SilentTyping: Plugin = {
-   ...manifest,
+  ...manifest,
 
-   onStart() {
-      Patcher.instead(Typing, 'startTyping', () => { });
-      Patcher.instead(Typing, 'stopTyping', () => { });
-   },
+  onStart() {
+    Patcher.instead(Typing, 'startTyping', () => { });
+    Patcher.instead(Typing, 'stopTyping', () => { });
 
-   onStop() {
-      Patcher.unpatchAll();
-   },
+    const voiceClient = new Discord.VoiceClient();
 
-   getSettingsPanel({ settings }) {
-      return <Settings settings={settings} />;
-   }
+    voiceClient.on('ready', () => {
+      // The voice client is ready to send voice messages.
+    });
+  },
+
+  onStop() {
+    Patcher.unpatchAll();
+
+    // Close the voice client.
+    voiceClient.destroy();
+  },
+
+  getSettingsPanel({ settings }) {
+    return (
+      <Settings settings={settings}>
+        <button onClick={async () => {
+          const audioFile = await window.openFile();
+
+          const voiceMessage = await this.uploadAudioFile(audioFile);
+
+          // Send the voice message.
+          voiceClient.play(voiceMessage);
+        }}>
+          Upload Audio File
+        </button>
+      </Settings>
+    );
+  },
+
+  async uploadAudioFile(audioFile: string): Promise<Discord.VoiceMessage> {
+    // Convert the audio file to a Discord voice message.
+    const voiceMessage = new Discord.VoiceMessage();
+    voiceMessage.setAudio(audioFile);
+
+    return voiceMessage;
+  }
 };
 
-registerPlugin(SilentTyping);
+registerPlugin(AudioUpload);
