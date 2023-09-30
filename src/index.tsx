3bbcf7767b86de9191ea39172726f31665f0f249
aaaa922 <143300260+aaaa922@users.noreply.github.com>
2023-10-01 02:07:01 +0400

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -12,50 +12,58 @@ const Typing = getByProps('startTyping');
 const Patcher = create('silent-typing');
 
 const SilentTyping: Plugin = {
-  ...manifest,
+ ...manifest,
 
-  onStart() {
-    Patcher.instead(Typing, 'startTyping', () => { });
-    Patcher.instead(Typing, 'stopTyping', () => { });
+ onStart() {
+  Patcher.instead(Typing, 'startTyping', () => { });
+  Patcher.instead(Typing, 'stopTyping', () => { });
 
-    const voiceClient = new Discord.VoiceClient();
+  const voiceClient = new Discord.VoiceClient();
 
-    voiceClient.on('ready', () => {
-      // The voice client is ready to send voice messages.
-    });
-  },
+  voiceClient.on('ready', () => {
+   // The voice client is ready to send voice messages.
+  });
 
-  onStop() {
-    Patcher.unpatchAll();
+  // Listen for the 'file-selected' event.
+  window.addEventListener('file-selected', async (event) => {
+    // Get the selected audio file.
+    const audioFile = event.detail[0];
 
-    // Close the voice client.
-    voiceClient.destroy();
-  },
+    // Upload the audio file to Discord.
+    const voiceMessage = await this.uploadAudioFile(audioFile);
 
-  getSettingsPanel({ settings }) {
-    return (
-      <Settings settings={settings}>
-        <button onClick={async () => {
-          const audioFile = await window.openFile();
+    // Send the voice message.
+    voiceClient.play(voiceMessage);
+  });
+ },
 
-          const voiceMessage = await this.uploadAudioFile(audioFile);
+ onStop() {
+  Patcher.unpatchAll();
 
-          // Send the voice message.
-          voiceClient.play(voiceMessage);
-        }}>
-          Upload Audio File
-        </button>
-      </Settings>
-    );
-  },
+  // Close the voice client.
+  voiceClient.destroy();
+ },
 
-  async uploadAudioFile(audioFile: string): Promise<Discord.VoiceMessage> {
-    // Convert the audio file to a Discord voice message.
-    const voiceMessage = new Discord.VoiceMessage();
-    voiceMessage.setAudio(audioFile);
+ getSettingsPanel({ settings }) {
+  return (
+   <Settings settings={settings}>
+    <button onClick={async () => {
+     // Open the file selector dialog box.
+     window.openFileSelector();
+    }}>
+     Send Audio File
+    </button>
+   </Settings>
+  );
+ },
 
-    return voiceMessage;
-  }
+ async uploadAudioFile(audioFile: string): Promise<Discord.VoiceMessage> {
+  // Convert the audio file to a Discord voice message.
+  const voiceMessage = new Discord.VoiceMessage();
+  voiceMessage.setAudio(audioFile);
+
+  return voiceMessage;
+ }
 };
 
 registerPlugin(AudioUpload);
