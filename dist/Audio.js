b5f0165a1f67afa3ae75109c62bdf28c29b10017
aaaa922 <143300260+aaaa922@users.noreply.github.com>
2023-10-01 01:06:22 +0400

    Update Audio.js

--- dist/Audio.js
+++ dist/Audio.js
@@ -1,1 +1,8 @@
-1
+// Import the Enmity library.
+const Enmity = require('enmity');
+
+// Get the module for your plugin.
+const plugin = require('./plugin');
+
+// Register the plugin with Enmity.
+Enmity.registerPlugin(plugin);
